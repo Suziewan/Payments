c2c58a0330f28234da0457c2dfcf1ce2644e0974
Suziewan <angielinton@me.com>
2017-01-13 17:02:25 -0500

    Initial Commit

---
+++ Payments/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Payments
+//
+//  Created by Angie Linton on 2017-01-13.
+//  Copyright © 2017 Angie Linton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
